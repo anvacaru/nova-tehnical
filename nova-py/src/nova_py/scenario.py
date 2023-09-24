647dac0c5b9d9ebf660613f5d7ba571538d524c4
unknown <andrei96v@gmail.com>
2023-09-24 22:21:45 +0300

    add Walk scenario

--- nova-py/src/nova_py/scenario.py
+++ nova-py/src/nova_py/scenario.py
@@ -24,18 +24,45 @@ SEASCAPE_IMGNAMES: Final[list[str]] = [
     'user_walking_back.png',
 ]
 
+WALK_COMPOSITION_DIR: Final[Path] = BASE_DIR / 'walk' / 'walk'
+WALK_IMGNAMES: Final[list[str]] = [
+    'P1 full body frontal.png',
+    'P1 full body side.png',
+    'P1 full body walking front.png',
+    'P2 full body frontal.png',
+    'P2 full body side.png',
+    'P2 full body walking front.png',
+]
 
-def create_scenario(timecues: dict[int, list[int]], img_names: list[str], output: Path) -> Dict[str, Any]:
-    return {'audio_map': timecues, 'img_names': img_names, 'output': output}
+
+def create_scenario(
+    timecues: dict[int, list[int]] | None, img_names: list[str], output: Path, has_visual: bool, has_audio: bool
+) -> Dict[str, Any]:
+    return {
+        'audio_map': timecues,
+        'img_names': img_names,
+        'output': output,
+        'has_visual': has_visual,
+        'has_audio': has_audio,
+    }
 
 
 SEASCAPE_SCENARIO: Final = create_scenario(
-    timecues=SEASCAPE_TIMECUES, img_names=SEASCAPE_IMGNAMES, output=SEASCAPE_COMPOSITION_DIR
+    timecues=SEASCAPE_TIMECUES,
+    img_names=SEASCAPE_IMGNAMES,
+    output=SEASCAPE_COMPOSITION_DIR,
+    has_visual=True,
+    has_audio=True,
+)
+
+WALK_SCENARIO: Final = create_scenario(
+    timecues=None, img_names=WALK_IMGNAMES, output=WALK_COMPOSITION_DIR, has_visual=True, has_audio=False
 )
 
 
 class Scenario(Enum):
     SEASCAPE = SEASCAPE_SCENARIO
+    WALK = WALK_SCENARIO
 
     def __str__(self) -> str:
         return self.name.lower()
